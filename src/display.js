a8e40841cc31ea24e6f1c0d18511efa775dfe2d8
NMRSantos <170880035+NMRSantos@users.noreply.github.com>
2026-03-18 19:26:32 +0000

    Fix form inputs being appended to content

--- src/display.js
+++ src/display.js
@@ -4,57 +4,58 @@ function renderForm() {
   const content = document.getElementById("content");
 
   const form = document.createElement("form");
+  form.id = "form";
   form.setAttribute("novalidate", true);
   content.appendChild(form);
 
   const emailLabel = document.createElement("label");
   emailLabel.setAttribute("for", "email");
   emailLabel.textContent = "Email:";
-  content.appendChild(emailLabel);
+  form.appendChild(emailLabel);
   const emailInput = document.createElement("input");
   emailInput.id = "email";
   emailInput.setAttribute("mandatory", true);
-  content.appendChild(emailInput);
+  form.appendChild(emailInput);
 
   const postalCodeLabel = document.createElement("label");
   postalCodeLabel.setAttribute("for", "postal-code");
   postalCodeLabel.textContent = "Postal Code:";
-  content.appendChild(postalCodeLabel);
+  form.appendChild(postalCodeLabel);
   const postalCodeInput = document.createElement("input");
   postalCodeInput.id = "postal-code";
   postalCodeInput.setAttribute("mandatory", true);
-  content.appendChild(postalCodeInput);
+  form.appendChild(postalCodeInput);
 
   const countryLabel = document.createElement("label");
   countryLabel.setAttribute("for", "country");
   countryLabel.textContent = "Country:";
-  content.appendChild(countryLabel);
+  form.appendChild(countryLabel);
   const countryInput = document.createElement("input");
   countryInput.id = "country";
   countryInput.setAttribute("mandatory", true);
-  content.appendChild(countryInput);
+  form.appendChild(countryInput);
 
   const passwordLabel = document.createElement("label");
   passwordLabel.setAttribute("for", "password");
   passwordLabel.textContent = "Password:";
-  content.appendChild(passwordLabel);
+  form.appendChild(passwordLabel);
   const passwordInput = document.createElement("input");
   passwordInput.id = "password";
   passwordInput.setAttribute("mandatory", true);
-  content.appendChild(passwordInput);
+  form.appendChild(passwordInput);
 
   const passwordValidationLabel = document.createElement("label");
-  content.appendChild(passwordValidationLabel);
   passwordValidationLabel.setAttribute("for", "password-validation");
   passwordValidationLabel.textContent = "Confirm password:";
+  form.appendChild(passwordValidationLabel);
   const passwordValidationInput = document.createElement("input");
   passwordValidationInput.id = "password-validation";
   passwordValidationInput.setAttribute("mandatory", true);
-  content.appendChild(passwordValidationInput);
+  form.appendChild(passwordValidationInput);
 
   const submitBtn = document.createElement("button");
   submitBtn.textContent = "Submit";
-  content.appendChild(submitBtn);
+  form.appendChild(submitBtn);
 
   return {
     emailInput,
